370ad1a410bb6d35c327db02fe2280c89c221dbe
Jesse Dahir-Kanehl <daisysgreenhouse@gmail.com>
2014-11-17 21:41:47 -0800

    Added pausing and changed comments

--- ringtone.asm
+++ ringtone.asm
@@ -1,5 +1,6 @@
-# MIPS Merge Sort
-# Jesse Dahir-Kanehl and Jonathan Loeffelholz
+# MIPS Ringtone
+# Jesse Dahir-Kanehl
+# based on jingle bells first two lines
 		
 		.data
 amoPrompt:	.asciiz "Enter the number of times to play the song: "	#prompt for the number of elements
@@ -27,9 +28,13 @@ MAIN:  		la $a0, amoPrompt					#load prompt
 		lw $s1, 0($s1)						#load value of array size into $s2
 		la $s2, pitch						#load address of pitch array start
 		la $s3, duration					#load address of duration array start
+		addi $a0, $zero, 5000					#pauses for 5 seconds
+		addi $v0, $zero, 32					# set up to pause
+		syscall							# pause
+FOR1:		beq $s0, $zero, END					# repetitions == 0 go to end
 		teq $zero, $zero					#immediately trap because $zero == $zero
-		
-		
+		addi $s0, $s0, -1					# $s0--
+		j FOR1							#loop
 END:		addi $v0, $zero, 10					#system call for exit
 		syscall							#clean termination of program
 	
@@ -43,7 +48,7 @@ END:		addi $v0, $zero, 10					#system call for exit
 		addi $a3, $zero, 90					#use volume level of 50
 		addi $v0, $zero, 33					#load service number 33 for MIDI synchronous
 		addi $s4, $zero, 0					# i = 0
-FOR:		slt $t0, $s4, $s1					# i < pitchnum
+FOR2:		slt $t0, $s4, $s1					# i < pitchnum
 		beq $t0, $zero, E					# i >= pitchnum go to E
 		sll $t0, $s4, 2						#multiply index by 4
 		add $t0, $t0, $s2					#add to pitch base address
@@ -53,7 +58,7 @@ FOR:		slt $t0, $s4, $s1					# i < pitchnum
 		lw $a1, 0($t0)						#get the current duration into $a1
 		syscall							#play the note
 		addi $s4, $s4, 1					#i++
-		j FOR							#loop
+		j FOR2							#loop
 E:		add $v0, $zero, $k0					#restore $v0
 		add $a0, $zero, $k1					#restore $a0
 		mfc0 $k0, $14						#CP0 reg. $14 is addr. of trapping inst.
